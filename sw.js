a569e31ae580bd62d5ae6862c50d419373977e60
pexa0n2k-hub <pexa0n2k@gmail.com>
2026-08-21 04:57:26 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="glass-notes-v04";
+const CACHE="glass-notes-v05";
 const ASSETS=["./","./index.html","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
